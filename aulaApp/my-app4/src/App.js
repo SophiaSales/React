c798de2f828c70d33d90104df1df13285ff928dd
SophiaSales <sophiabrenda10@gmail.com>
2021-05-14 16:55:03 -0300

    aula41

---
+++ aulaApp/my-app4/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './App.css';
+
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <h3 className="cor-texto">Texto da minha aplicação</h3>
+      </div>
+    )
+  }
+}
+
+export default App;
